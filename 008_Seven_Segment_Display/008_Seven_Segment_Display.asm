cafdfa7b564c3e58020313606b3e77532d6b00e9
Onur ALACA <onuralcaa@gmail.com>
2024-01-13 13:18:56 +0300

    Seven segment 0-15

--- 008_Seven_Segment_Display/008_Seven_Segment_Display.asm
+++ 008_Seven_Segment_Display/008_Seven_Segment_Display.asm
@@ -1,26 +1,25 @@
-;Sayaç ile seven segment disp kullanýmý
+;D portune baðlý 8 adet led ile binary sayma iþlemi
 
 	include "P16F877.INC"
 
-	org 0x100
+	ORG 0x00
 
-bcf STATUS, RP1
-bsf STATUS, RP0
-clrf TRISB
+BCF STATUS, RP1
+BSF STATUS, RP0
+CLRF TRISB
+BCF STATUS, RP0 ;bank0 seçildi
+CLRF PORTB
 
-bcf STATUS, RP0
-clrf PORTB
-
-SAY EQU H'20'
-N EQU H'21'
-M EQU H'22'
+SAY EQU 0x20
+N EQU 0x21
+M EQU 0x22
 
 MOVLW H'00'
 MOVWF SAY
 
 DEVAM ;ANADÖNGÜ
-	MOVF SAY,W ;SAY DEÐERÝNÝ W YE ATA : W=SAY
-	ANDLW B'00001111' ;SAYININ(W) 15 Ý GEÇMEMESÝNÝ SAÐLAYAN AND ÝÞLEMÝ
+	MOVF SAY,W ;SAY DEÐERÝNÝ W YE KOPYALA/ATA : W=SAY
+	ANDLW B'00001111' ;SAYININ(W) 15 i GEÇMEMESÝNÝ SAÐLAYAN AND ÝÞLEMÝ
 
 	CALL SEVSEG
 	MOVWF PORTB ;PORTA GÖNDER
@@ -30,37 +29,39 @@ DEVAM ;ANADÖNGÜ
 
 GOTO DEVAM
 
-	SEVSEG
+;GECÝKME FONK.(alt program)
+GECÝK 
+	movlw .255 
+	movwf N
+	say1
+	movlw .255 
+	movwf M
+	say2
+		decfsz M,F
+	goto say2
+	decfsz N,F		
+
+	goto say1
+return
+
+SEVSEG
 		ADDWF PCL,F
 		;Sayýlarýn asci kodlarý
-		RETLW H'3F'
-		RETLW H'06'
-		RETLW H'5B'
-		RETLW H'4F'
-		RETLW H'66'
-		RETLW H'6D'
-		RETLW H'7D'
-		RETLW H'07'
-		RETLW H'7F'
-		RETLW H'6F'
-		RETLW H'77'
-		RETLW H'7C'
-		RETLW H'39'
-		RETLW H'5E'
-		RETLW H'79'
-		RETLW H'71'
-		
-GECÝK
-	movlw d'255'
-	movwf N
-	SAY1
-		MOVLW d'255'
-		MOVWF M
-		SAY2
-			DECFSZ M,F
-		GOTO SAY2
-		DECFSZ N,F
-	GOTO SAY1	
-
-RETURN
+		RETLW H'3F' ;0
+		RETLW H'06' ;1
+		RETLW H'5B' ;2
+		RETLW H'4F' ;3
+		RETLW H'66' ;4
+		RETLW H'6D' ;5
+		RETLW H'7D' ;6
+		RETLW H'07' ;7
+		RETLW H'7F' ;8
+		RETLW H'6F' ;9
+		RETLW H'77' ;A
+		RETLW H'7C' ;b
+		RETLW H'39' ;C
+		RETLW H'5E' ;D
+		RETLW H'79' ;e
+		RETLW H'71' ;F
+ 
 END
