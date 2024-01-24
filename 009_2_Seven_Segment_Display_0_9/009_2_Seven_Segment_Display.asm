0acc23ac382556226b3f1b459f0f6a52a28cbbb8
Onur ALACA <onuralcaa@gmail.com>
2024-01-24 21:47:49 +0300

    0-9 counter

--- 009_2_Seven_Segment_Display_0_9/009_2_Seven_Segment_Display.asm
+++ 009_2_Seven_Segment_Display_0_9/009_2_Seven_Segment_Display.asm
@@ -1,74 +1,61 @@
-;B ve C portune baðlý birer seven segment disp ile 0-99 arasý sayýcý
+include "P16F877.INC"
 
-	include "P16F877.INC"
-
-	ORG 0x00
+    ORG 0x00
 
 BCF STATUS, RP1
 BSF STATUS, RP0
 CLRF TRISC
-BCF STATUS, RP0 ;bank0 seçildi
+BCF STATUS, RP0 ; Bank 0 seçildi
 CLRF PORTC
+CLRF SAY ; Sayac deðiþkeni sýfýrlanýyor
 
-BCF STATUS, RP1
-BSF STATUS, RP0
-CLRF TRISB
-BCF STATUS, RP0 ;bank0 seçildi
-CLRF PORTB
+SAY EQU 0x20 ; Sayac deðiþkeni
+N EQU 0x21
+M EQU 0x22
 
-SAY EQU 0x20 ;Birler basamaðý
-SAY2 EQU 0x21 ;Onlar basama
-N EQU 0x22
-M EQU 0x23
+DEVAM: 
+MOVF SAY,W  
+CALL SEVSEG
+MOVWF PORTC
+CALL GECIK   
 
+INCF SAY,F  
 
-DEVAM ;ANADÖNGÜ
-	
-	INCF SAY,F
-	MOVF SAY,W ;SAY DEÐERÝNÝ W YE KOPYALA/ATA : W=SAY
-	SUBLW d'10'
-	BTFSS STATUS,Z
-	GOTO DISP
-	MOVLW h'00'	
-	MOVWF SAY
-	GOTO DISP
-	GOTO DEVAM
-		
- 
-	DISP
-	CALL SEVSEG
-	MOVWF PORTB	
-	
-	CALL GECÝK
+MOVF SAY,W
+SUBLW 0x10  
+BTFSS STATUS,Z
+GOTO DEVAM
+CLRF SAY
 
 GOTO DEVAM
 
-;GECÝKME FONK.(alt program)
-GECÝK 
-	movlw .255 
-	movwf N
-	say1
-	movlw .255 
-	movwf M
-	say2
-		decfsz M,F
-	goto say2
-	decfsz N,F		
 
-	goto say1
-return
+; GECIKME FONKSIYONU (alt program)
+GECIK 
+    movlw .255 
+    movwf N
+DONGU1
+    movlw .255 
+    movwf M
+DONGU2
+    decfsz M,F        
+    goto DONGU2
+    decfsz N,F        
+    goto DONGU1
+    return
 
+
+; 7 SEGMENT KOD ÇEVÝRME FONKSIYONU (alt program)
 SEVSEG
-		ADDWF PCL,F
-		;Sayýlarýn asci kodlarý	
-		RETLW H'6F' ;9
-		RETLW H'7F' ;8
-		RETLW H'07' ;7
-		RETLW H'7D' ;6
-		RETLW H'6D' ;5
-		RETLW H'66' ;4
-		RETLW H'4F' ;3
-		RETLW H'5B' ;2
-		RETLW H'06' ;1
-		RETLW H'3F' ;0
+        ADDWF PCL,F
+        RETLW 0x3F ; 0
+        RETLW 0x06 ; 1
+        RETLW 0x5B ; 2
+        RETLW 0x4F ; 3
+        RETLW 0x66 ; 4
+        RETLW 0x6D ; 5
+        RETLW 0x7D ; 6
+        RETLW 0x07 ; 7
+        RETLW 0x7F ; 8
+        RETLW 0x6F ; 9
 END
