a53050dbf5622d6051b85cd0e1532ae05a12de77
Onur ALACA <onuralcaa@gmail.com>
2024-01-25 14:22:26 +0300

    completed.

--- 005_1_Button_binary_control/Button_binary_control.asm
+++ 005_1_Button_binary_control/Button_binary_control.asm
@@ -1,42 +1,44 @@
-;D portune baðlý 8 adet led ile binary sayma iþlemi
+include "P16F877.INC"
+;B portuna baðlý buton ile D portuna baðlý 8 adet ledi binary þekilde kontrol etmek.
+org 0x00
 
-	include "P16F877.INC"
+; PORT ayarlarý ve deðiþkenler
+    bcf STATUS, RP1
+    bsf STATUS, RP0     ; Bank 1 seçimi
+    clrf TRISD          ; PORTD'yi çýkýþ olarak ayarla
+    bsf TRISB, 0        ; RB0 pinini giriþ olarak ayarla
+    bcf STATUS, RP0     ; Bank 0'a geri dön
+    clrf PORTD          ; PORTD'yi sýfýrla
 
-	org 0x00
-
-bcf STATUS, RP1
-bsf STATUS, RP0
-clrf TRISD
-bcf STATUS, RP0 ;bank0 seçildi
-clrf PORTD
 N equ 0x20
 M equ 0x21
 
-devam;anadongu
-	movlw h'00'
-	movwf PORTD
-	say
-		call gecikme
-		incfsz PORTD, F ;bir arttýr, sýfýr kontrolü yap (0 olduðunda 2 alt satýra geçer)
-	goto say
+ANADONGU:
+    btfss PORTB, 0     ; Buton kontrolü (RB0)
+    goto ANADONGU     ; Buton basýlmadýysa döngüye devam et
+    call GECIK         ; Buton basýlýysa gecikme fonksiyonunu çaðýr
+    incf PORTD, F      ; PORTD'deki deðeri 1 arttýr
 
+button_kontrol:
+    btfsc PORTB, 0     ; Butonun býrakýldýðýný kontrol et
+    goto button_kontrol ; Buton býrakýlana kadar bekle
+    call GECIK         ; Buton býrakýldýðýnda gecikme fonksiyonunu tekrar çaðýr
+    goto ANADONGU     ; Ana döngüye geri dön
 
-GOTO devam
+; GECIK alt programý
+GECIK:
+    movlw D'50'       ; N için baþlangýç deðeri
+    movwf N
+SAY1:
+    movlw D'50'       ; M için baþlangýç deðeri
+    movwf M
 
-;GECÝKME FONK.(alt program)
-gecikme 
-	movlw .255 
-	movwf N
-	say1
-	movlw .255 
-	movwf M
-	say2
-		decfsz M,F
-	goto say2
-	decfsz N,F		
+;TEK BEKLEME YETERLÝ GÖRÜLDÜ.
+;SAY2:
+    ;decfsz M, F        ; M'yi azalt ve sýfýr olup olmadýðýný kontrol et
+    ;goto SAY2    ; M sýfýr deðilse döngüye devam et
+    decfsz N, F        ; N'yi azalt ve sýfýr olup olmadýðýný kontrol et
+    goto SAY1    ; N sýfýr deðilse döngüye devam et
+    return             ; Gecikme tamamlandý, fonksiyondan çýk
 
-	goto say1
-return
-
- 
-END
+end                     ; Program sonu
